946d6fab0acfae6e175c553026ddb2fb3fe3a404
MRayhanAP <nxy929@gmail.com>
2020-10-15 15:27:37 +0700

    initial commit

---
+++ DemoSocketClient1841720121Rey/varGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSocketClient1841720121Rey
+{
+    class varGlobal
+    {
+        public static string alamatIPServer;
+        public static int port;
+        public static string terimapesandariserver;
+        public static string terimapesandiserver;
+
+    }
+}
